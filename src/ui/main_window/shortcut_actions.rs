6a7287adc85ff1fad78ea31e4d3e86935255c3aa
Ksiw <ken939@mail.ru>
2026-03-29 22:57:13 +0300

    Add drawing primitive and brush size hotkeys

--- src/ui/main_window/shortcut_actions.rs
+++ src/ui/main_window/shortcut_actions.rs
@@ -1,4 +1,5 @@
 use crate::app::PixelPaintApp;
+use crate::domain::DrawPrimitive;
 use eframe::egui;
 
 pub fn handle_shortcuts(ctx: &egui::Context, app: &mut PixelPaintApp) {
@@ -21,6 +22,18 @@ pub fn handle_shortcuts(ctx: &egui::Context, app: &mut PixelPaintApp) {
         !text_input_active && ctx.input(|i| i.modifiers.command && i.key_pressed(egui::Key::S));
     let save_as_pressed = !text_input_active
         && ctx.input(|i| i.modifiers.command && i.modifiers.shift && i.key_pressed(egui::Key::S));
+    let line_primitive_pressed =
+        !text_input_active && ctx.input(|i| i.key_pressed(egui::Key::Q));
+    let point_primitive_pressed =
+        !text_input_active && ctx.input(|i| i.key_pressed(egui::Key::W));
+    let size_1_pressed =
+        !text_input_active && ctx.input(|i| i.key_pressed(egui::Key::Num1));
+    let size_2_pressed =
+        !text_input_active && ctx.input(|i| i.key_pressed(egui::Key::Num2));
+    let size_3_pressed =
+        !text_input_active && ctx.input(|i| i.key_pressed(egui::Key::Num3));
+    let size_4_pressed =
+        !text_input_active && ctx.input(|i| i.key_pressed(egui::Key::Num4));
 
     if reset_zoom_pressed {
         app.editor.zoom = 1.0;
@@ -49,5 +62,29 @@ pub fn handle_shortcuts(ctx: &egui::Context, app: &mut PixelPaintApp) {
         super::file_actions::save_document_as(app);
     } else if save_pressed {
         super::file_actions::save_document(app);
+    }
+    if line_primitive_pressed {
+        app.editor.primitive = DrawPrimitive::Line;
+        app.mark_session_dirty();
+    }
+    if point_primitive_pressed {
+        app.editor.primitive = DrawPrimitive::Point;
+        app.mark_session_dirty();
+    }
+    if size_1_pressed {
+        app.editor.size_index = 0;
+        app.mark_session_dirty();
+    }
+    if size_2_pressed {
+        app.editor.size_index = 1;
+        app.mark_session_dirty();
+    }
+    if size_3_pressed {
+        app.editor.size_index = 2;
+        app.mark_session_dirty();
+    }
+    if size_4_pressed {
+        app.editor.size_index = 3;
+        app.mark_session_dirty();
     }
 }
